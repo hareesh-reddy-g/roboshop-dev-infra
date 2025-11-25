c9023d84408e49fc76be11b98cc480ae98374a6c
Hareesh Reddy <hareesh.iamreddy@gmail.com>
2025-11-25 16:16:14 +0530

    ansible-roboshop-roles with terraform

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -124,7 +124,7 @@ resource "aws_instance" "mysql" {
     instance_type = "t3.micro"
     vpc_security_group_ids = [local.mysql_sg_id]
     subnet_id = local.database_subnet_id
-    iam_instance_profile = aws_iam_instance_profile.mysql
+    iam_instance_profile = aws_iam_instance_profile.mysql.name
     
     tags = merge (
         local.common_tags,
