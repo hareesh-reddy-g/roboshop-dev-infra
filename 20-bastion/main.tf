2f2f485639d2c5f86db375026e99af6ad6d37b59
Hareesh Reddy <hareesh.iamreddy@gmail.com>
2025-11-28 11:43:20 +0530

    roboshop dev infra with terraform

--- 20-bastion/main.tf
+++ 20-bastion/main.tf
@@ -3,8 +3,9 @@ resource "aws_instance" "bastion" {
     instance_type = "t3.micro"
     vpc_security_group_ids = [local.bastion_sg_id]
     subnet_id = local.public_subnet_id
-    
+    iam_instance_profile = aws_iam_instance_profile.bastion.name
     user_data = file("bastion.sh")
+    
     tags = merge (
         local.common_tags,
         {
@@ -14,6 +15,7 @@ resource "aws_instance" "bastion" {
 }
 
 
-
-
-
+resource "aws_iam_instance_profile" "bastion" {
+    name = "bastion"
+    role = "BastionTerraformAdmin"
+}
